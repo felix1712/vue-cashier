14d40a2a46f2918fca7493c3687de550647d11f6
felix <idn_felix.andrean>
2019-02-18 11:31:10 +0700

    move store vuex to folder because it will modular

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules:{ // modules must be included in import
+  },
+
+  state: { //data
+		
+  },
+  getters: { //computed property
+  	
+  },
+  mutations: {
+  	
+  },
+  actions: { //methods property
+  	//make the call
+  	//call mutations
+  },
+});
