e7e69e34a5722d7a8c1cbe4a5a7a2bcfdb796423
felix <idn_felix.andrean>
2019-02-18 11:27:50 +0700

    add some configuration in es lint js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "indent": [2, "tab"],
+    "no-tabs": 0,
+    "semi": [2, "never"],
   },
   parserOptions: {
     parser: 'babel-eslint',
