508c0587a4524aa2f9386e0f7f66fdec78f98d08
felix <idn_felix.andrean>
2019-02-20 16:17:25 +0700

    fixing eslint error in file

--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,17 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules:{ // modules must be included in import
-  },
+	modules: { // modules must be included in import
+	},
 
-  state: { //data
-		
-  },
-  getters: { //computed property
-  	
-  },
-  mutations: {
-  	
-  },
-  actions: { //methods property
-  	//make the call
-  	//call mutations
-  },
+	state: { // data
+	},
+	getters: { // computed property
+	},
+	mutations: {
+	},
+	actions: { // methods property
+		// make the call
+		// call mutations
+	},
 });
