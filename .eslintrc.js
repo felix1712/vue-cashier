1df307ae7fefdbe175e0553b4eecd71b4db16576
felix <idn_felix.andrean>
2019-02-20 16:09:16 +0700

    add semicolon rules on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "indent": [2, "tab"],
     "no-tabs": 0,
-    "semi": [2, "never"],
+    // "semi": [2, "never"], this rules is for remove semicolon on eslint
   },
   parserOptions: {
     parser: 'babel-eslint',
