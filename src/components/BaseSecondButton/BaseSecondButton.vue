fbfbe6bf5b76b31edf6ca0d8f4e9b1eb30cc5067
felix <idn_felix.andrean>
2019-02-22 16:41:56 +0700

    button styling

---
+++ src/components/BaseSecondButton/BaseSecondButton.vue
@@ -0,0 +1,28 @@
+<template src="./index.html"></template>
+
+<script>
+export default {
+	name: 'BaseSecondButton',
+	computed: {
+		type() {
+			if(this.to){
+				return 'router-link'
+			}
+
+			return 'a'
+		},
+	},
+	props: {
+		title: {
+			type:String,
+			required: true,
+		},
+		to: {
+			required: false,
+		},
+		typeButton: {
+			required: false,
+		},
+	},
+};
+</script>
