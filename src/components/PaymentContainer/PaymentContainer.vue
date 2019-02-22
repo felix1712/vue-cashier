67068e00d5c1140e8c18b9c12614a810958f3ba6
felix <idn_felix.andrean>
2019-02-22 10:48:05 +0700

    fix linting error and import base progress

--- src/components/PaymentContainer/PaymentContainer.vue
+++ src/components/PaymentContainer/PaymentContainer.vue
@@ -1,7 +1,12 @@
 <template src="./index.html"></template>
 
 <script>
+import BaseProgress from '@/components/BaseProgress/BaseProgress.vue'
+
 export default {
 	name: 'PaymentContainer',
-}
+	components: {
+		BaseProgress
+	}
+};
 </script>
