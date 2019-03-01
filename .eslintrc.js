5932adc3346144da19fd0fe1487a360d4086b2ff
felix <idn_felix.andrean>
2019-03-01 16:40:17 +0700

    linting fixing error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "indent": [2, "tab"],
-    "no-tabs": 0,
-    "no-useless-escape": 0,
-    "no-param-reassign": 0
-    // "semi": [2, "never"], this rules is for remove semicolon on eslint
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'@vue/airbnb',
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+		'no-useless-escape': 0,
+		'no-param-reassign': 0,
+		// "semi": [2, "never"], this rules is for remove semicolon on eslint
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
 };
