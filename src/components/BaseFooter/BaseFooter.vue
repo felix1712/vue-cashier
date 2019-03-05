2ae23750924061b5c1d89f8b85e97684b6a3d72b
felix <idn_felix.andrean>
2019-03-05 13:14:11 +0700

    Base footer styling

--- src/components/BaseFooter/BaseFooter.vue
+++ src/components/BaseFooter/BaseFooter.vue
@@ -13,30 +13,34 @@ export default {
 
 <style lang="scss" scoped>
 	#baseFooter{
-		.wrapper-btn {
-			background-image: linear-gradient(0deg, #EC4848 0%, #CB2E36 50%);
-			box-shadow: 3px 3px 4px 0 rgba(184,19,19,0.50);
-			border-radius: 3px;
-			padding: 1%;
-			margin-top: 1%;
-			margin-bottom: 1%;
-			text-align: center;
-		}
-		.confrimation-color-text {
-			color: white;
-			font-size: 18px;
-			color: #FFFFFF;
-		}
-		.text-price {
-			font-size: 20px;
-			color: #CB2E36;
-			text-align: right;
-		}
+		.base-footer-frame{
+			.wrapper-btn {
+				background-image: linear-gradient(0deg, #EC4848 0%, #CB2E36 50%);
+				box-shadow: 3px 3px 4px 0 rgba(184,19,19,0.50);
+				border-radius: 3px;
+				padding: 1%;
+				margin-top: 1%;
+				margin-bottom: 1%;
+				text-align: center;
+				cursor: pointer;
+			}
+			.confrimation-color-text {
+				color: white;
+				font-size: 18px;
+				color: #FFFFFF;
+			}
+			.text-price {
+				font-size: 20px;
+				color: #CB2E36;
+				text-align: right;
+			}
 
-		.wrapper-text {
+			.wrapper-text {
 				text-align: right;
 				padding-right: 14%;
 				padding-top: 2%;
+				font-weight: bold;
+			}
 		}
 	}
 </style>
