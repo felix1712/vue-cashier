5c819ad3952f60a769fc4623cadd14013b1b3d47
felix <idn_felix.andrean>
2019-03-06 17:39:53 +0700

    frame item MF

---
+++ src/components/ItemMf/ItemMf.vue
@@ -0,0 +1,14 @@
+<template src="./index.html"></template>
+
+<script>
+import BaseRadioPeriod from '@/components/BaseRadioPeriod/BaseRadioPeriod.vue';
+import BaseFormGroup from '@/components/BaseFormGroup/BaseFormGroup.vue';
+
+export default {
+	name: 'ItemMf',
+	components: {
+		BaseRadioPeriod,
+		BaseFormGroup,
+	},
+};
+</script>
---
+++ src/store/modules/multiFinance.js
@@ -0,0 +1,34 @@
+export default {
+	state: {
+		bankArray: [
+				{
+					id: 11,
+					name: 'Bank Central Asia',
+					nameAbbr: 'BCA',
+					image: 'https://img11.360buyimg.com/Indonesia/jfs/t1/11452/19/10248/2033/5c7e486bE45e02b27/d87a3eb7199d38b7.png',
+				},
+
+				{
+					id: 12,
+					name: 'Bank Negara Indonesia',
+					nameAbbr: 'BNI',
+					image: 'https://img11.360buyimg.com/Indonesia/jfs/t1/10334/33/13138/2163/5c7e48c6E474fc8d4/7065ec18c705b387.png',
+				},
+
+				{
+					id: 13,
+					name: 'Bank Rakyat Indonesia',
+					nameAbbr: 'BRI',
+					image: 'https://img11.360buyimg.com/Indonesia/jfs/t1/26808/11/9535/1447/5c7e48adE20d04d72/130e39e3e3224f01.png',
+				},
+
+
+			],x
+	},
+	mutations: {
+
+	},
+	actions: {
+
+	},
+};
